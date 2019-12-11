4e0b908fd6af73c2f7da74aed73751987132d952
Alves Kévin <kevin_alves@sma-groupe.fr>
2019-12-11 11:43:06 +0100

    enabling AppProperties

--- src/main/java/com/social_signin/springsocial/SpringSocialApplication.java
+++ src/main/java/com/social_signin/springsocial/SpringSocialApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class SpringSocialApplication {
 
     public static void main(String[] args) {
