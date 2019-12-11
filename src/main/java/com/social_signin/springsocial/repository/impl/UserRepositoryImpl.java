c98c18140b8547320de9d6f94149985ea3ff0b0e
Alves Kévin <kevin_alves@sma-groupe.fr>
2019-12-11 13:57:49 +0100

    add user Repository with JPA

---
+++ src/main/java/com/social_signin/springsocial/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.social_signin.springsocial.repository.impl;
+
+import com.social_signin.springsocial.repository.UserRepository;
+
+abstract class UserRepositoryImpl implements UserRepository {
+}
