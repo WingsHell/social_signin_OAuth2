f70b31ecc930366671e08c4264551396c6485f71
Alves Kévin <kevin_alves@sma-groupe.fr>
2019-12-11 14:55:42 +0100

    implements user and facebook factory class

---
+++ src/main/java/com/social_signin/springsocial/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -0,0 +1,26 @@
+package com.social_signin.springsocial.security.oauth2.user;
+
+import com.social_signin.springsocial.exception.OAuth2AuthenticationProcessingException;
+import com.social_signin.springsocial.model.AuthProvider;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        /*if(registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
+            return new FacebookOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
+            return new GithubOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+        }*/
+        if(registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
+            return new FacebookOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+        }
+    }
+}
