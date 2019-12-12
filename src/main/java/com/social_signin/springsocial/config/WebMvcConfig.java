5d2f967bb8a1291dfc8357a1b2c391d43856578e
Alves Kévin <kevin_alves@sma-groupe.fr>
2019-12-12 12:16:33 +0100

    add authorization and content-type to allowedHeaders

--- src/main/java/com/social_signin/springsocial/config/WebMvcConfig.java
+++ src/main/java/com/social_signin/springsocial/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedHeaders("Authorization, Content-Type")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
     }
